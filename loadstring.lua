a0f4c7601e10001fe46d634e740d40019aa88e62
WaWa <142501996+wawa2009@users.noreply.github.com>
2026-03-12 21:56:35 +0700

    Update URL in loadstring for Output.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wawa2009/Jailbreak-script/refs/heads/main/Output.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wawa2009/Jailbreak-script/refs/heads/main/Output.lua?token=GHSAT0AAAAAADWDYPUMXNTDJ3SIAEJ5TYDW2NS2QYQ"))()
