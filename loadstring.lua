84aec9ccf7e286c8e2b85b3249ff3e9db2d2e6f0
WaWa <142501996+wawa2009@users.noreply.github.com>
2026-03-14 13:02:34 +0700

    Update URL in loadstring to remove unnecessary query

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wawa2009/Jailbreak-script/refs/heads/main/Output.lua%20(1).txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wawa2009/Jailbreak-script/refs/heads/main/Output.lua"))()
